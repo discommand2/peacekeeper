9a17c6560fd87ceab76d62716e7da9864308a912
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-05-27 03:36:21 -0400

    Update PeaceKeeper.php

--- src/PeaceKeeper.php
+++ src/PeaceKeeper.php
@@ -2,10 +2,15 @@
 
 namespace Discommand2\PeaceKeeper;
 
+use DiscordPHP\DiscordPHP;
+
 Class PeaceKeeper {
+
+
     public function __construct() {
         echo "PeaceKeeper is here to keep the peace!\n";
     }
+
     public function run() {
         echo "PeaceKeeper is running!\n";
     }
